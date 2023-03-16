82bf9ea51e4f3d5f85d59bcd0e133390e2ceacae
小井 <58876476+2676547504@users.noreply.github.com>
2023-03-16 20:23:30 +0800

    Add files via upload

---
+++ 状态栏透明.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+%1 start "" mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit /y
+echo  ‰»Î y ∏≤∏«º¥ø…
+REG ADD HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v TaskbarAcrylicOpacity /t REG_DWORD /d 0
