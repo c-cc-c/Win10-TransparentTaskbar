c8e26c6d96f203da08d9d34a16b93f7f9a9aaaef
小井 <58876476+c-cc-c@users.noreply.github.com>
2023-06-11 01:57:43 +0800

    Update 状态栏透明.bat
    Only the title has been changed in this update

--- 状态栏透明.bat
+++ 状态栏透明.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
+title win10状态栏一键透明
 %1 start "" mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit /y
 echo 按 y 覆盖即可
 REG ADD HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v TaskbarAcrylicOpacity /t REG_DWORD /d 0
