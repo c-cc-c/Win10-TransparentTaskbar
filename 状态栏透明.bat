ab1505d31ebc513a4310c286a50d6549bdf6fe21
小井 <58876476+c-cc-c@users.noreply.github.com>
2023-06-19 01:01:40 +0800

    Update 状态栏透明.bat
    无需手动，点击即可完成

--- 状态栏透明.bat
+++ 状态栏透明.bat
@@ -1,5 +1,5 @@
-@ECHO OFF
+@echo off
 title win10状态栏一键透明
 %1 start "" mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit /y
-echo 按 y 覆盖即可
-REG ADD HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v TaskbarAcrylicOpacity /t REG_DWORD /d 0
+REG ADD HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v EnableTransparency /t REG_DWORD /d 1 /f
+REG ADD HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v TaskbarAcrylicOpacity /t REG_DWORD /d 0 /f
